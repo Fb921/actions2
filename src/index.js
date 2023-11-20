bdb29e74dc19185e2dddf5e6498685f8e40a55aa
Bintou <fofana_bintou@outlook.fr>
2023-11-20 13:46:07 +0100

    Release

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import App from './App.js';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals.js';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
